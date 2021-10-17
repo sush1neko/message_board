b074258df511a6644040fe55c2ae1b547877c695
aika.horikawa <magnoliaika@gmail.com>
2021-10-17 13:37:06 +0900

    GetAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,27 +7,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="messages")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
+@Table(name = "messages")
 public class Message {
     @Id
-    @Column(name="id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
-    @Column(name="title", length=255, nullable=false)
+    @Column(name = "title", length = 255, nullable = false)
     private String title;
 
-    @Column(name="content", length=255, nullable=false)
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
-    @Column(name="created_at", nullable=false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name="updated_at", nullable=false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
     public Integer getId() {
--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,20 +5,19 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-        private static final String PERSISTENCE_UNIT_NAME ="message_board";
-        private static EntityManagerFactory emf;
+    private static final String PERSISTENCE_UNIT_NAME = "message_board";
+    private static EntityManagerFactory emf;
 
-        public static EntityManager createEntityManager() {
-            return getEntityManagerFactory().createEntityManager();
+    public static EntityManager createEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
 
-        private static EntityManagerFactory getEntityManagerFactory() {
-            if(emf==null) {
-                emf =Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-            }
-
-            return emf;
-
+        return emf;
     }
 
 }
