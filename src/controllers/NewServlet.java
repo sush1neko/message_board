1997aeecea64072206686b709bbae04fbe32d782
aika.horikawa <magnoliaika@gmail.com>
2021-10-17 19:56:59 +0900

    CreateServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -23,23 +23,19 @@ public class NewServlet extends HttpServlet {
      */
     public NewServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //CSRF対策
+        // CSRF対策
         request.setAttribute("_token", request.getSession().getId());
 
-        //おまじないとしてのインスタンスを生成
-        request.setAttribute("message",  new Message());
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
 
-        RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
         rd.forward(request, response);
-
-
-
      }
 }
